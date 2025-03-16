1c436fd87d526c067f168bbf37d9a4334b1f04b7
Victoria Khoreva <victheliar@gmail.com>
2025-03-16 13:22:21 +0200

    Update db.py

--- db.py
+++ db.py
@@ -2,10 +2,10 @@ import sqlite3
 from flask import g
 
 def get_connection():
-con = sqlite3.connect("database.db")
-con.execute("PRAGMA foreign_keys = ON")
-con.row_factory = sqlite3.Row
-return con
+	con = sqlite3.connect("database.db")
+	con.execute("PRAGMA foreign_keys = ON")
+	con.row_factory = sqlite3.Row
+	return con
 
 def execute(sql, params =[]):
 	con = get_connection()
