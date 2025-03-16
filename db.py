30707d5ed9e74260ebdd704d887b4825c667e659
Victoria Khoreva <victheliar@gmail.com>
2025-03-16 13:24:39 +0200

    Update db.py

--- db.py
+++ db.py
@@ -2,24 +2,24 @@ import sqlite3
 from flask import g
 
 def get_connection():
-	con = sqlite3.connect("database.db")
-	con.execute("PRAGMA foreign_keys = ON")
-	con.row_factory = sqlite3.Row
-	return con
+    con = sqlite3.connect("database.db")
+    con.execute("PRAGMA foreign_keys = ON")
+    con.row_factory = sqlite3.Row
+    return con
 
 def execute(sql, params =[]):
-	con = get_connection()
-	result = con.execute(sql, params)
-	con.commit()
-	g.last_insert_id = result.lastrowid
-	con.close()
+    con = get_connection()
+    result = con.execute(sql, params)
+    con.commit()
+    g.last_insert_id = result.lastrowid
+    con.close()
 
 def last_insert_id():
-	return g.last_insert_id
+    return g.last_insert_id
 
 def query(sql, params=[]):
-	con = get_connection()
-	result = con.execute(sql, params).fetchall()
-	con.close()
-	return result
+    con = get_connection()
+    result = con.execute(sql, params).fetchall()
+    con.close()
+    return result
 
