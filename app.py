79ccc1f24d217d0e7d133a62df962778f78dec77
Victoria Khoreva <victheliar@gmail.com>
2025-04-04 14:18:02 +0300

    Refactor registration/login

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import sqlite3
 from flask import Flask
 from flask import redirect, render_template, request, session, make_response, abort
-from werkzeug.security import generate_password_hash
-from werkzeug.security import check_password_hash
 import db
 import config
 import items
@@ -163,11 +161,9 @@ def create():
 
     if password1 != password2:
         return "VIRHE: salasanat eivät ole samat"
-    password_hash = generate_password_hash(password1)
 
     try:
-        sql = "INSERT INTO Users (username, password_hash) VALUES (?, ?)"
-        db.execute(sql, [username, password_hash])
+        users.create_user(username, password1)
     except sqlite3.IntegrityError:
         return "VIRHE: tunnus on jo varattu"
 
@@ -182,12 +178,8 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
 
-        sql = "SELECT id, password_hash FROM Users WHERE username = ?"
-        result = db.query(sql, [username])[0]
-        user_id = result["id"]
-        password_hash = result["password_hash"]
-
-        if check_password_hash(password_hash, password):
+        user_id = users.check_login(username, password)
+        if user_id:
             session["user_id"] = user_id
             session["username"] = username
             return redirect("/")
--- users.py
+++ users.py
@@ -1,7 +1,9 @@
 import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 def get_user(user_id):
-    sql = """SELECT id, username, profile_picture IS NOT NULL has_pfp
+    sql = """SELECT id, username,
+    profile_picture IS NOT NULL has_pfp
     FROM Users
     WHERE id = ?"""
     result = db.query(sql, [user_id])
@@ -25,3 +27,23 @@ def get_profile_picture(user_id):
     sql = "SELECT profile_picture FROM Users WHERE id = ?"
     result = db.query(sql, [user_id])
     return result[0][0] if result else None
+
+def create_user(username, password1):
+    password_hash = generate_password_hash(password1)
+    sql = "INSERT INTO Users (username, password_hash) VALUES (?, ?)"
+    db.execute(sql, [username, password_hash])
+
+def check_login(username, password):
+    sql = "SELECT id, password_hash FROM Users WHERE username = ?"
+    result = db.query(sql, [username])
+
+    if not result:
+        return None
+    print("testi", result)
+    user_id = result[0]["id"]
+    password_hash = result[0]["password_hash"]
+
+    if check_password_hash(password_hash, password):
+        return user_id
+    else:
+        return None
