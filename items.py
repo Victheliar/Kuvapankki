77624462b12b59dfff148d13fc438fbadd12bcdf
Victoria Khoreva <victheliar@gmail.com>
2025-04-19 20:55:15 +0300

    Fix adding images

--- items.py
+++ items.py
@@ -61,7 +61,7 @@ def get_items(page, page_size):
 def get_image(item_id):
     sql = "SELECT images FROM Items WHERE id = ?"
     result = db.query(sql, [item_id])    
-    return result[0]["images"] if result else None
+    return result[0][0] if result else None
 
 def get_item(item_id):
     sql = """SELECT Items.id, Items.description,
